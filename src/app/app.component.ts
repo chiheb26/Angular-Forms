5decf2e5c8e2dacc93d2744071975f6b801b16c1
chiheb <chiheb.fellah@gmail.com>
2022-12-22 01:41:02 +0100

    FormControlGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,30 @@ import{NgForm} from '@angular/forms'
 export class AppComponent {
   title = 'AngularForms';
   @ViewChild("myForm")
-  form?:NgForm;
+  form!:NgForm;
   defaultCountry:string="india";
   firstname?:string;
   lastname?:string;
   mail?:string;
+  gender=[
+    {id:'1',value:"Male"},
+    {id:'2',value:"Female"},
+    {id:'3',value:"Other"}
+  ];
+  defaultGender:string="Male";
+  hobbies:any[]=[
+    {id:0,value:"Sports",selected:false},
+    {id:1,value:"Movies",selected:true},
+    {id:2,value:"Music",selected:false}
+  ];
 
-
+  toggleCheckbox(id:any){
+    this.hobbies[id].selected=!this.hobbies[id].selected;
+  }
   //onSubmit(form:NgForm){
   onSubmit(){
     console.log(this.form);
+    console.log(this.hobbies);
   }
 
 }
